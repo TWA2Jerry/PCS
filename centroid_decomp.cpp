0bd733749b9ef35aa22c7ca997815f11b8eb778a
TWA2Jerry <jzhou224@gmail.com>
2019-09-18 23:22:31 +0800

    just checking

--- centroid_decomp.cpp
+++ centroid_decomp.cpp
@@ -2,7 +2,6 @@
 
 using namespace std;
 
-int N, no_edges;
 
 void dfs(int s, int e, vector<int> * adj, int * sizes){
 
@@ -47,15 +46,16 @@ int find_centroid(int u, int n, vector<int> * adj, int * sizes, bool * seen){
 
 
 int main(void){
-
-	scanf("%d %d", &N, &no_edges);
+	
+	int N;
+	scanf("%d", &N);
 		
 	int subtree_sizes[N];
 	vector <int> adj[N];
 	vector<int> centroid_tree[N];
 	bool seen[N];
 
-	for(int i = 0; i<N; i++){
+	for(int i = 0; i < N; i++){
 		seen[i] = false;
 		subtree_sizes[i] = 0;
 	}
@@ -66,27 +66,19 @@ int main(void){
 		adj[a].push_back(b);
 		adj[b].push_back(a);
 	}
-	printf("Finished scanning\n");
 
 	dfs(N-1, -1, adj, subtree_sizes);	
-	printf("Finished dfs\n");
-	for(int i = 0; i<N; i++){
-		printf("%d\n", subtree_sizes[i]);
-	}	
 
 
 	int centroid = find_centroid(N-1, N, adj, subtree_sizes, seen);
 	seen[centroid] = true;
-	printf("Found centroid: %d\n", centroid);
 	queue<int> q;
 	q.push(centroid);	
 
 	while(!q.empty()){
 		int u = q.front(); q.pop();
-		printf("Centroid %d: ", u);
 		for(auto s : adj[u]){
 			if(!seen[s]){
-				printf("%d ", s);
 				int subtree_centroid  = find_centroid(s, subtree_sizes[s], adj,subtree_sizes, seen);
 				q.push(subtree_centroid);
 				seen[subtree_centroid] = true;
@@ -95,31 +87,8 @@ int main(void){
 				centroid_tree[subtree_centroid].push_back(u);
 			}
 		}
-		printf("\n");	
 	}
 	
-	//Now getting ready to print
-	q.push(centroid);
-	for(int i = 0; i<N; i++){
-		seen[i] = false;
-	}	
-
-	printf("Final Tree:\n");	
-	
-	printf("%d\n", centroid);
-	seen[centroid] = true;
-	while(!q.empty()){
-		int u = q.front(); q.pop();
-		for(auto s:centroid_tree[u]){
-			if(seen[s]){
-				continue;
-			}
-			q.push(s);
-			seen[s] = true;
-			printf(" %d ", s);
-		}
-		printf("\n");
-	}
 
 	printf("All done\n");
 	return 0;
